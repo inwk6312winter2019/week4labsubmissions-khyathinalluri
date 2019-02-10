a58d7b1ea741d9a1dcb86fea8480aafd9d9a2880
Khyathinalluri <Khyathi.Nalluri@dal.ca>
2019-02-10 06:34:42 -0800

    second  program part 1 of lab5

---
+++ task2.py
@@ -0,0 +1,26 @@
+import math
+class point(object):
+	"""this is a class used to find distance between two points"""
+
+def print_point(self):
+		print("(%g,%g)"%(self.x,self.y))
+
+
+
+class rectangle():
+	"""this is a class for recatngle"""
+
+def find_center(self):
+	p = point()
+	p.x=self.center.x
+	p.y=self.center.y
+	return p
+
+
+ds=rectangle()
+ds.center=point()
+ds.center.x=0.0
+ds.center.y=0.0
+center=find_center(ds)
+print_point(center)
+
